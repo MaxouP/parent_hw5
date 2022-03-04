acf84985f645d45d17f15e6ff977ed3045c773a3
Maxence Parent <maxence.parent@epitech.eu>
2022-03-03 16:34:45 -0800

    improvement

--- js/ex2.js
+++ js/ex2.js
@@ -1,19 +1,9 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const odd = array.filter((val) => val % 2 != 0);
-console.log(odd);
+console.log(array.filter((val) => val % 2 != 0));
 
-const divBy2o5 = array.filter((val) => val % 2 == 0 || val % 5 == 0);
-console.log(divBy2o5);
+console.log(array.filter((val) => val % 2 == 0 || val % 5 == 0));
 
-const divBy3 = [];
-array.map((val) => {
-	if (val % 3 == 0) {
-		divBy3.push(Math.pow(val, 2));
-	}
-});
-console.log(divBy3);
+console.log(array.filter((val) => val % 3 === 0).map((val) => Math.pow(val, 2)));
 
-const sqrDivBy5 = array.filter((val) => val % 5 == 0);
-const sum = sqrDivBy5.reduce((prev, curr) => Math.pow(prev, 2) + Math.pow(curr, 2));
-console.log(sum);
+console.log(array.filter((val) => val % 5 == 0).reduce((prev, curr) => Math.pow(prev, 2) + Math.pow(curr, 2)));
